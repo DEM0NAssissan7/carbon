9d0c4c9737737ccb3950585c6d590c550f976916
DEM0NAssissan7 <abdurahman.elmawi06@gmail.com>
2023-05-02 12:45:07 -0500

    add gilbert

---
+++ applications/graphics/gilbert.js
@@ -0,0 +1,18 @@
+
+class Gilbert {
+    point(x, y) {
+        return {x: x,
+                y: y};
+    }
+    object(x, y) {
+        let GilbertObject = function() {
+            this.points = [];
+            this.fill = "black";
+            this.shader = () => {}
+        };
+        GilbertObject.prototype.render = function(x, y) {
+
+        }
+        return result;
+    }
+}
