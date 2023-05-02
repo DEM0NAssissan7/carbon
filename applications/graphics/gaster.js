aae76ff5a543444f01c921f0748fe568e2033847
DEM0NAssissan7 <abdurahman.elmawi06@gmail.com>
2023-05-02 12:44:56 -0500

    add gaster

---
+++ applications/graphics/gaster.js
@@ -0,0 +1,15 @@
+/*  Gaster i a 3D Raytracing graphics engine. Usable for games or rendering as needed.
+    Similar to Gilbert, except using pathtracing instead of raycasting and intelligent shading.
+*/
+let GasterObject = function() {
+    this.points = [];
+    this.fill = "white";
+}
+class Gaster {
+    constructor() {
+        this.objects = [];
+    }
+    create_object(){
+
+    }
+}
